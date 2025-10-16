ca4f635bad78f744b18d449b0ba5c6448d0e3168
Chirag Raju <chiragrajus2002@gmail.com>
2025-10-16 22:58:10 +0530

    COMMIT-8 Angular Get All products and models

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,12 +1,30 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, OnDestroy, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './layout/header/header.component';
+import { HttpClient } from '@angular/common/http';
+import { Products } from './shared/models/Product';
+import { Pagination } from './shared/models/Pagination';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: '../tailwind.css',
 })
-export class AppComponent {
-  protected title = 'client';
+export class AppComponent implements OnInit, OnDestroy {
+  baseUrl = 'http://localhost:5089/api/';
+  private http = inject(HttpClient);
+  protected title = 'Automobile';
+
+  products: Products[] = [];
+
+  ngOnInit(): void {
+    this.http.get<Pagination<Products>>(this.baseUrl + 'products').subscribe({
+      next: (products) => (this.products = products.data),
+      error: (error) => console.log(error),
+      complete: () => console.log('Request completed'),
+    });
+  }
+
+  ngOnDestroy(): void {}
 }
---
+++ client/src/app/shared/models/Product.ts
@@ -0,0 +1,10 @@
+export type Products={
+    id:number;
+    name:string;
+    description:string;
+    price:number;
+    type:string;
+    brand:string;
+    quantityInStock:number;
+    pictureUrl:string;
+}
